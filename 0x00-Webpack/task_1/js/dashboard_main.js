70529d1f6b5d11e25c3bc88f58001bdc94603ab2
nja93 <njanjalchege@gmail.com>
2024-01-03 00:37:00 +0300

    use webpack with config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<button>Holberton Dashboard</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+//bind debounce function to click event (every -500 millisecond)
+$('button').on('click', _.debounce(updateCounter, 500));
+
+//initialize count variable of click events
+let count = 0;
+
+//update the number of click events and print
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
