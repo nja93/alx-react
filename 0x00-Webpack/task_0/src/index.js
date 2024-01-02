f281068d3d4be632caeccbb1e8a30a857d52b938
nja93 <njanjalchege@gmail.com>
2024-01-02 23:01:54 +0300

    removed function

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 
-$(document).ready(function () {
-  // Add paragraphs to the page body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
