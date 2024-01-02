0c43e46e2219b99381e7ef3a3304cc12f3f8d065
nja93 <njanjalchege@gmail.com>
2024-01-03 00:49:34 +0300

    removed unnecessary tags

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>button>Click here to get started</button<p>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
