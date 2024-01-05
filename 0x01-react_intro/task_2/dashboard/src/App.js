a06c10614be6a1fe9096afe731295b6f17b1ad5e
nja93 <njanjalchege@gmail.com>
2024-01-05 13:15:58 +0300

    added form for passwrod and email

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import HolbertonLogo from '../src/assets/Holberton-Logo.jpg';
+import './App.css';
+import { getFooterCopy } from './utils';
+import { getFullYear } from './utils';
+
+function App() {
+  const isIndex = true;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= {HolbertonLogo} alt="Holberton-logo" className='App-logo'/>
+        <h1 className='headertext'>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <h1>Login to access the full dashboard</h1>
+        <form>
+          <label htmlFor='email'>Email: </label>
+          <input type='email' id="email" name="name"/>
+
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password"/>
+          <button> OK </button>
+        </form>
+      </body>
+      <footer className="App-footer">Copyright {getFullYear()} - {getFooterCopy(isIndex)}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
