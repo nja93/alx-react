8b6004e0cbb1642ae03435e4bb4d95451feab84f
nja93 <njanjalchege@gmail.com>
2024-01-07 17:01:28 +0300

    basic applicatio created by react

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import HolbertonLogo from './assets/Holberton-Logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= {HolbertonLogo} alt="Holberton-logo" className='App-logo'/>
+        <h1 className='headertext'>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <h1>Login to access the full dashboard</h1>
+      </body>
+      <footer className="App-footer">Copyright 2020 - holberton School
+      </footer>
+    </div>
+  );
+}
+
+export default App;
