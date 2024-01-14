022855b4768f564f5823536c002a16739eed4aa7
nja93 <njanjalchege@gmail.com>
2024-01-14 20:33:24 +0300

    hanglidn events

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -1,65 +1,81 @@
-import React, {Component } from 'react';
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import CourseList from "../CourseList/CourseList";
-import PropTypes from "prop-types";
-import "./App.css";
-import { getLatestNotification } from "../utils/utils";
-
-const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React", credit: 40}
-]
-const listNotifications = [
-  {id: 1, type:'default', value:"New course available"},
-  {id: 2, type:'urgent', value:"New resume available"},
-  {id: 3, type:'urgent', html:{__html: getLatestNotification() }}
-]
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notification from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
 
 
-class App extends Component {
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress)
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.isLoggedIn = props.isLoggedIn;
+    this.logOut = props.logOut;
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.listCourses = [
+      {id: 1, name: 'ES6', credit: 60},
+      {id: 2, name: 'Webpack', credit: 20},
+      {id: 3, name: 'React', credit: 40}
+    ];
+  
+    this.listNotifications = [
+      {id: 1, value: "New course available", type: "default"},
+      {id: 2, value: "New resume available", type: "urgent"},
+      {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+    ];
   }
-  handleKeyPress = (e) => {
+
+  handleKeyDown(e) {
     if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault();
       alert("Logging you out");
-      this.props.logOut()
-    }
+      this.logOut();
+    }  
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress)
-  }
-    render () {
-      return(
-      <div>
-      <Notifications listNotifications={listNotifications} />
-      <div className='App'>
-        <Header />
-        <div className='App-body'>
-        {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        </div>
-        <div className='App-footer'>
-          <Footer />
-        </div>
-      </div>
-      </div>
-  );
-}
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
 
-  App.defaultProps = {
-    isLoggedIn: false,
-    logOut: () => {return }
+  render () {
+    return (
+      <React.Fragment>
+        <Notification listNotifications={this.listNotifications}/>
+        <div className="App">
+          <Header />
+          {this.props.isLoggedIn ?
+            <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={this.listCourses}/></BodySectionWithMarginBottom>
+          : 
+            <BodySectionWithMarginBottom title="Log in to continue"><Login /></BodySectionWithMarginBottom>
+          }
+          <BodySection title="News from the School">
+            <p>Random Text</p>
+          </BodySection>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
   }
-  
-  App.propTypes = {
-    isLoggedIn: PropTypes.bool,
-    logOut: PropTypes.func
-  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
 
 export default App;
