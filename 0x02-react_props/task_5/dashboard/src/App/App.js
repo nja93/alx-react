6c6e82d6c3877ba95eb98f24c0db55603e3c440a
nja93 <njanjalchege@gmail.com>
2024-01-14 17:50:00 +0300

    fixed export default statement

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -6,15 +6,29 @@ import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 import "./App.css";
+import { getLatestNotification } from "../utils/utils";
+
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: getLatestNotification() },
+];
 
 export default function App({ isLoggedIn }) {
   return (
     <>
-      <Notifications />
+      <Notifications listNotifications={listNotifications} />
       <div className='App'>
         <Header />
         <div className='App-body'>
-          {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         </div>
         <div className='App-footer'>
           <Footer />
