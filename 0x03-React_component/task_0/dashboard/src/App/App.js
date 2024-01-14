1c69eb8bf1a779f11480df2d1cb1589998945dd4
nja93 <njanjalchege@gmail.com>
2024-01-14 18:08:32 +0300

    Convert the App function into a React Class

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,52 @@
+import React, {Component } from 'react';
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+import "./App.css";
+import { getLatestNotification } from "../utils/utils";
+
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 }
+]
+
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html:{__html: getLatestNotification() }}
+]
+
+
+  class App extends Component {
+    render () {
+      return(
+      <div>
+      <Notifications listNotifications={listNotifications} />
+      <div className='App'>
+        <Header />
+        <div className='App-body'>
+        {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        </div>
+        <div className='App-footer'>
+          <Footer />
+        </div>
+      </div>
+      </div>
+  );
+}
+  }
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+export default App;
