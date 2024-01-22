e2bcaccd10af1f3238a3e2e7d01e063ec6e9ee20
nja93 <njanjalchege@gmail.com>
2024-01-23 02:35:55 +0300

    attempt inlne styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React from 'react'; 
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const rowBgColor = isHeader ? '#deb5b545' : '#f5f5f5ab';
+
+  return (
+    <tr style={{ backgroundColor: rowBgColor }}>
+      {isHeader ? (
+        textSecondCell ? (
+          <>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </>
+        ) : (
+          <th colSpan={2}>{textFirstCell}</th>
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+export default CourseListRow;
---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+
+describe('<CourseListRow />', () => {
+  it('renders one cell with colspan = 2 when textSecondCell does not exist (isHeader=true)', () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell='Course name'
+        textSecondCell={null}
+      />
+    );
+    const cell = wrapper.find('th');
+    expect(cell).toHaveLength(1);
+    expect(cell.prop('colSpan')).toBe(2);
+  });
+
+  it('renders two cells when textSecondCell is present (isHeader=true)', () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell='Course name'
+        textSecondCell={!null}
+      />
+    );
+    expect(wrapper.find('th')).toHaveLength(2);
+  });
+
+  it('renders correctly two td elements within a tr element (isHeader=false)', () => {
+    const wrapper = shallow(
+      <CourseListRow isHeader={false} textFirstCell='Course name' />
+    );
+    const row = wrapper.find('tr');
+    expect(row.find('td')).toHaveLength(2);
+  });
+});
